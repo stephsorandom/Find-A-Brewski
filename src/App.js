c0bd0a627d5ca32305da6819fdfd14820b95997d
Stephanie Mitchell <stephsorandom@gmail.com>
2021-02-19 12:13:34 -0500

    styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, BrowserRouter as Router, Switch} from 'react-router-dom'
+import {Link, BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Main from './Components/Main'
 
@@ -7,9 +7,9 @@ import Main from './Components/Main'
 function App() {
   return (
     <div>
-
+  <h1 style={{fontFamily:"monospace", textShadow:"2px 2px 1px green"}}>Find a Brewski</h1>
   <Main />
-
+{/* <Route path="/results" component={} />      */}
     </div>
   );
 }
