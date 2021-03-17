84d7877a5c3d1e19d7b1b721b4e38d8c5e4594f1
Stephanie Mitchell <stephsorandom@gmail.com>
2021-03-16 23:59:31 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import Main from "./Components/Main";
+import React from "react"
+import "./App.css"
+import Main from "./Components/Main"
 
 function App() {
   return (
