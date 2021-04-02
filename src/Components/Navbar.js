05da94a79315d1951da7435a1bb348da91fd14f0
Stephanie Mitchell <stephsorandom@gmail.com>
2021-04-02 09:45:16 -0400

    erase random Hello on homescreen

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Navbar(props) {
+    const [isOpen, setOpen] = useState(false);
     return (
         <div>
-            
-
+            <nav
+            className="navbar is-primary"
+            role="navigation"
+            aria-label="main navigation"
+            >
+            <div className="container">
+            <div className="navbar-brand">
+                <a
+                    role="button"
+                    className={`navbar-burger burger ${isOpen && "is-active"}`}
+                    aria-label="menu"
+                    aria-expanded="false"
+                    onClick={() => setOpen(!isOpen)}
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+                </div>
+                {/* ... */}
+            </div>
+            </nav>
+  );
 
 
             
