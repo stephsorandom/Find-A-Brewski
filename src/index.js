95bed4249041bbc2b16235dc3937123b2dfbdd23
Stephanie Mitchell <stephsorandom@gmail.com>
2021-12-20 23:46:13 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,6 +12,7 @@ ReactDOM.render(
     </div>
   </BrowserRouter>,
   document.getElementById("root")
+
 );
 
 // If you want to start measuring performance in your app, pass a function
